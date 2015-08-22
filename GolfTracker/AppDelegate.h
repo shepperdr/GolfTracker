7a4d0423ac721211bd65ecf84c329948e6d0b3ce
Robert Shepperd <shepperdr@hotmail.com>
2015-08-22 01:14:09 -0600

    First commit

---
+++ GolfTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GolfTracker
+//
+//  Created by Robert Shepperd on 8/22/15.
+//  Copyright (c) 2015 Robert Shepperd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
